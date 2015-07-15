e2627881f5ed6694ebba5496b5b26a82d5c7ba45
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-15 23:33:23 +0100

    Updated client config.

--- src/resources/client_config.py
+++ src/resources/client_config.py
@@ -1,1 +1,7 @@
+# Run in debug mode for useful error messages
 DEBUG = True
+
+# DB config - FWIW I don't like this being here since it's exposed to the templating
+# engine and could thus accidentally end up being printed to the client, but apparently
+# Flask-SQLAlchemy expects this config to be defined...
+SQLALCHEMY_DATABASE_URI = "mysql://root:fake@localhost/hextechprojectx"
