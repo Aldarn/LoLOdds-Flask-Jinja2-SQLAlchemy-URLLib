cf25ebde595be80d779f106ce9843d3856d23a2e
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 21:31:18 +0100

    Added profile icon base url config.

--- src/resources/config.py
+++ src/resources/config.py
@@ -1,2 +1,6 @@
 # LoL API key
 API_KEY = "db60a3be-abf0-479e-b99f-99515a164e55"
+
+# URLs
+# TODO: These shouldn't be hardcoded but should instead at least use the API to get the latest versions / links
+PROFILE_ICON_BASE_URL = "http://ddragon.leagueoflegends.com/cdn/5.13.1/img/profileicon/"
