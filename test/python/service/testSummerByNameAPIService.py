f9ae2bc8a26936e3eb6951f0083fa47bc8f065f8
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 20:45:38 +0100

    Added tests for summoner by name api service.

---
+++ test/python/service/testSummerByNameAPIService.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python2.7
+
+import unittest
+from mock import patch, Mock, MagicMock
+from src.api.summoner.summoner_by_name import SUMMONER_BY_NAME
+
+class TestSummonersAPIService(unittest.TestCase):
+	@patch.object(SUMMONER_BY_NAME, '_getData')
+	def testGetSummoners(self, getDataMock):
+		# -------------------------------------------------------
+		SUMMONER_BY_NAME.getSummoner("benholmes")
+		# -------------------------------------------------------
+		getDataMock.assert_called_with(endpoint = "benholmes")
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
