bbc2f137bc59b57598c514661df9db95424ca0ef
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 19:02:15 +0100

    Added fail-fast tests for the daemon.

---
+++ test/python/daemon/daemon.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env/python2.7
+
+import unittest
+import src.api_daemon.daemon
+
+class TestDaemon(unittest.TestCase):
+	def testDaemonFetchesFeaturedGames(self):
+		# -------------------------------------------------------
+
+		# -------------------------------------------------------
+		self.fail("bla")
+
+	def testDaemonProcessesAllFeaturedGames(self):
+		# -------------------------------------------------------
+
+		# -------------------------------------------------------
+		self.fail("bla")
+
+	def testDaemonRespectsClientRefreshInterval(self):
+		# -------------------------------------------------------
+
+		# -------------------------------------------------------
+		self.fail("bla")
+
+	def testDaemonHandlesFailure(self):
+		# -------------------------------------------------------
+
+		# -------------------------------------------------------
+		self.fail("failure not handled")
+
+	def testDaemonHandlesEmptyResults(self):
+		# -------------------------------------------------------
+
+		# -------------------------------------------------------
+		self.fail("empty results not handled")
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
