57e53b2300dfdb64f2b46688eb1b6432af17b30c
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 12:01:16 +0100

    Added RankedStats service.

---
+++ src/api/stats/ranked_stats.py
@@ -0,0 +1,26 @@
+from src.api.api_service import APIService
+
+class RankedStats(APIService):
+	ENDPOINT_BASE = "https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/"
+	ENDPOINT = "/ranked"
+
+	VALID_SEASONS = ("SEASON3", "SEASON2014", "SEASON2015")
+
+	def __init__(self):
+		super(RankedStats, self).__init__(RankedStats.ENDPOINT_BASE)
+
+	def getStats(self, summonerId, season = None):
+		if season and season in RankedStats.VALID_SEASONS:
+			self._getData("%s%s" % (summonerId, RankedStats.ENDPOINT), season = season)
+		else:
+			# Use the default if no season is provided
+			self._getData("%s%s" % (summonerId, RankedStats.ENDPOINT))
+
+	def _onSuccess(self, result):
+		pass
+
+	def _onFail(self, result):
+		pass
+
+# Create a handle to this service
+RANKED_STATS = RankedStats()
