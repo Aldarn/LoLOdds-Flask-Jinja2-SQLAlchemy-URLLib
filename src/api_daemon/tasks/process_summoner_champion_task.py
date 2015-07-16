5cfdf2cb2dd7262bcfab604e290c324551237f67
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 00:09:57 +0100

    Fixed query and json keys in summoner champion task.

--- src/api_daemon/tasks/process_summoner_champion_task.py
+++ src/api_daemon/tasks/process_summoner_champion_task.py
@@ -1,7 +1,6 @@
 from task import Task
 from src.hextech_project_x import DB
 from src.domain.summoner_champion_stats import SummonerChampionStats
-from sqlalchemy import and_
 
 class ProcessSummonerChampionTask(Task):
 	def __init__(self, championJSON, summoner):
@@ -14,9 +13,12 @@ class ProcessSummonerChampionTask(Task):
 	def run(self):
 		championId = int(self.championJSON["id"])
 
+		print "championId %s" % championId
+		print "championJSON %s" % self.championJSON
+
 		# Check if these stats have already been recorded
-		currentChampionStats = SummonerChampionStats.query.filter_by(
-			and_(summonerId = self.summoner.summonerId, championId = championId)).first()
+		currentChampionStats = SummonerChampionStats.query.filter_by(summonerId = self.summoner.summonerId,
+			championId = championId).first()
 
 		# Update the existing one
 		if currentChampionStats:
@@ -32,9 +34,9 @@ class ProcessSummonerChampionTask(Task):
 		championImageUrl = "TODO"
 
 		summonerChampionStats = SummonerChampionStats(self.summoner.summonerId, championId, championImageUrl,
-			self.championJSON["totalSessionsWon"], self.championJSON["totalSessionsLost"])
+			self.championJSON["stats"]["totalSessionsWon"], self.championJSON["stats"]["totalSessionsLost"])
 
-		self.summoner.children.append(summonerChampionStats)
+		self.summoner.summonerChampionStats.append(summonerChampionStats)
 		DB.session.add(summonerChampionStats)
 		DB.session.commit()
 
@@ -42,6 +44,6 @@ class ProcessSummonerChampionTask(Task):
 	Updates an existing entry.
 	"""
 	def updateExistingChampionStats(self, currentChampionStats):
-		currentChampionStats.totalSessionsWon = self.championJSON["totalSessionsWon"]
-		currentChampionStats.totalSessionsLost = self.championJSON["totalSessionsLost"]
+		currentChampionStats.totalSessionsWon = self.championJSON["stats"]["totalSessionsWon"]
+		currentChampionStats.totalSessionsLost = self.championJSON["stats"]["totalSessionsLost"]
 		DB.session.commit()
