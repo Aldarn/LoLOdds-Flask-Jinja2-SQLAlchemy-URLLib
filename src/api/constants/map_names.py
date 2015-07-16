5414797eb943e50fe4362775480691b275f4b6f3
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 15:47:39 +0100

    Added map names constants.

---
+++ src/api/constants/map_names.py
@@ -0,0 +1,8 @@
+SUMMONERS_RIFT_ORIGINAL = 1 # Summoner's Rift Original Summer Variant
+SUMMONERS_RIFT_ORIGINAL_AUTUMN = 2 #Summoner's Rift Original Autumn Variant
+THE_PROVING_GROUNDS = 3 # The Proving Grounds Tutorial Map
+TWISTED_TREELINE_ORIGINAL = 4 # Twisted Treeline Original Version
+THE_CRYSTAL_SCAR = 8 # The Crystal Scar	Dominion Map
+TWISTED_TREELINE = 10 # Twisted Treeline Current Version
+SUMMONERS_RIFT = 11	# Summoner's Rift Current Version
+HOWLING_ABYSS = 12 # Howling Abyss
