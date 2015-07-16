8791462fd5365fe356ec070b0dc05a0d8d9ed38d
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 19:01:03 +0100

    Added indices to games domain object.

--- src/domain/games.py
+++ src/domain/games.py
@@ -3,11 +3,11 @@ from game_summoners import GAME_SUMMONERS
 
 class Games(DB.Model):
 	gameId = DB.Column(DB.Integer, primary_key = True, autoincrement = False)
-	gameMode = DB.Column(DB.String(50))
-	gameQueueId = DB.Column(DB.Integer)
-	gameType = DB.Column(DB.String(50))
-	mapId = DB.Column(DB.Integer)
-	platformId = DB.Column(DB.Integer)
+	gameMode = DB.Column(DB.String(50), index = True)
+	gameQueueId = DB.Column(DB.Integer, index = True)
+	gameType = DB.Column(DB.String(50), index = True)
+	mapId = DB.Column(DB.Integer, index = True)
+	platformId = DB.Column(DB.String(10), index = True)
 
 	summoners = DB.relationship('Summoners', secondary = GAME_SUMMONERS, backref = DB.backref('games', lazy='dynamic'))
 
