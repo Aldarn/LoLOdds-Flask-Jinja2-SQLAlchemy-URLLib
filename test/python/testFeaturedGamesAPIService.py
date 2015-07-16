bc1bd6ca08fe1797c2c332ded26f816829fd37c2
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 10:16:40 +0100

    Added placeholder for featured games api service tests.

---
+++ test/python/testFeaturedGamesAPIService.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python2.7
+
+import unittest
+
+class TestFeaturedGamesAPIService(unittest.TestCase):
+	def testThing(self):
+		# -------------------------------------------------------
+		# TODO
+		# -------------------------------------------------------
+		self.fail("what is it supposed to do?")
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
