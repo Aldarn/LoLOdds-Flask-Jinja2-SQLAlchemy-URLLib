66b59ad51c8b1106b4e677a0fb90a43478091072
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 18:59:55 +0100

    Remove on fail / success methods from featured games api service, added getFeaturedGames method.

--- src/api/featured_games/featured_games.py
+++ src/api/featured_games/featured_games.py
@@ -4,11 +4,8 @@ class FeaturedGames(APIService):
 	def __init__(self):
 		super(FeaturedGames, self).__init__("https://euw.api.pvp.net/observer-mode/rest/featured")
 
-	def _onSuccess(self, result):
-		pass
-
-	def _onFail(self, result):
-		pass
+	def getFeaturedGames(self):
+		return self._getData()
 
 # Create a handle to this service
 FEATURED_GAMES = FeaturedGames()
