290aaea4ab1455d8430428b7be526043da9b25c9
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 17:44:26 +0100

    Updated create_db script.

--- create_db.py
+++ create_db.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env/python2.7
 
+# Must import all DB models / tables
 from src.hextech_project_x import DB
+from src.domain.games import Games
+from src.domain.summoners import Summoners
+from src.domain.game_summoners import GAME_SUMMONERS
+from src.domain.summoner_champion_stats import SummonerChampionStats
 
-# SQLAlchemy will generate and run SQL from all our domain objects
+# Create & commit them
 DB.create_all()
+DB.session.commit()
