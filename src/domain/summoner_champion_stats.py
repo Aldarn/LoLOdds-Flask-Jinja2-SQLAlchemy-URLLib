e726eb73f2b3fd3875d23aa56189e9210d26316d
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-16 20:43:56 +0100

    Improved summoner champion stats domain object repr.

--- src/domain/summoner_champion_stats.py
+++ src/domain/summoner_champion_stats.py
@@ -3,7 +3,7 @@ from src.hextech_project_x import DB
 class SummonerChampionStats(DB.Model):
 	summonerId = DB.Column(DB.Integer, primary_key = True, autoincrement = False)
 	championId = DB.Column(DB.Integer, primary_key = True, autoincrement = False)
-	championImageUrl = DB.Column(DB.String(255))
+	championImageUrl = DB.Column(DB.String(255)) # This really really shouldn't be here as it will be duplicated for all summoners (amongst other reasons)
 	totalSessionsWon = DB.Column(DB.Integer)
 	totalSessionsLost = DB.Column(DB.Integer)
 
@@ -15,4 +15,4 @@ class SummonerChampionStats(DB.Model):
 		self.totalSessionsLost = totalSessionsLost
 
 	def __repr__(self):
-		return '<Summoner %i>' % self.summonerId
+		return '<Summoner Champion Stats %i, %i>' % (self.summonerId, self.championId)
