8de7d88791c189f8cfe2b8d088dd221b6d212719
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 00:21:45 +0100

    Fixed GameSummoners domain object foreign keys.

--- src/domain/game_summoners.py
+++ src/domain/game_summoners.py
@@ -1,8 +1,8 @@
 from src.hextech_project_x import DB
 
 class GameSummoners(DB.Model):
-    gameId = DB.Column(DB.BigInteger, primary_key = True, autoincrement = False)
-    summonerId = DB.Column(DB.BigInteger, primary_key = True, autoincrement = False)
+    gameId = DB.Column(DB.BigInteger, DB.ForeignKey('games.gameId'), primary_key = True, autoincrement = False)
+    summonerId = DB.Column(DB.BigInteger, DB.ForeignKey('summoners.summonerId'), primary_key = True, autoincrement = False)
     totalSessionsWon = DB.Column(DB.Integer)
     totalSessionsLost = DB.Column(DB.Integer)
     totalChampionSessionsWon  = DB.Column(DB.Integer)
@@ -11,7 +11,7 @@ class GameSummoners(DB.Model):
     championId = DB.Column(DB.Integer)
     championImageUrl = DB.Column(DB.String(255)) # TODO: This should be stored in its own image table to prevent redundancy
 
-    summoner = DB.relationship('Summoners', backref = DB.backref('GameSummoners', lazy='joined'), lazy='dynamic')
+    summoner = DB.relationship('Summoners', backref = DB.backref('GameSummoners', lazy='joined'), lazy='joined')
 
     def __init__(self, gameId, summonerId, totalSessionsWon, totalSessionsLost, totalChampionSessionsWon,
             totalChampionSessionsLost, teamId, championId, championImageUrl):
