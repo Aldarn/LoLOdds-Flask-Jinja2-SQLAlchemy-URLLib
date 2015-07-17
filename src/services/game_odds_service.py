b8dd74b22cb033f75b4c9630c3012b82efe57bfe
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 02:19:40 +0100

    Partially implemented GameOddsService.

--- src/services/game_odds_service.py
+++ src/services/game_odds_service.py
@@ -1,31 +1,65 @@
+from src.domain.games import Games
+import math
+from fractions import gcd
+
 class GameOddsService(object):
 	def __init__(self):
 		pass
 
-	def getGamesWithOdds(self):
-		return {
-			"games": [
-				{
-					"summoners": [
-						{
-							"name": "yo"
-						},
-						{
-							"name": "yo2"
-						},
-					]
-				},
-				{
-					"summoners": [
-						{
-							"name": "hey"
-						},
-						{
-							"name": "sup"
-						},
-					]
-				}
-			]
-		}
+	# def getGamesWithOdds(self):
+	# 	return {
+	# 		"games": [
+	# 			{
+	# 				"summoners": [
+	# 					{
+	# 						"name": "yo"
+	# 					},
+	# 					{
+	# 						"name": "yo2"
+	# 					},
+	# 				]
+	# 			},
+	# 			{
+	# 				"summoners": [
+	# 					{
+	# 						"name": "hey"
+	# 					},
+	# 					{
+	# 						"name": "sup"
+	# 					},
+	# 				]
+	# 			}
+	# 		]
+	# 	}
+
+	# def getGamesWithOdds(self):
+	# 	games = Games.query.all()
+	#
+	# 	gameList = []
+	# 	for game in games:
+	# 		summonersList = []
+	# 		teamWinsAndLosses = {}
+	# 		for summoner in game.summoners:
+	# 			summonersList.append({
+	# 				"name": summoner.name,
+	# 				"championImageUrl": thingy
+	# 			})
+	# 		gameList.append({ "summoners": summonersList })
+	#
+	# 	return { "games": gameList }
+
+	def _calculateGameOdds(self, redTotalWins, redTotalLosses, purpleTotalWins, purpleTotalLosses):
+		redWinPercentage = self._getPercentage(redTotalWins, redTotalLosses)
+		purpleWinPercentage = self._getPercentage(purpleTotalWins, purpleTotalLosses)
+
+		redChance = self._getPercentage(redWinPercentage, purpleWinPercentage)
+		purpleChance = self._getPercentage(purpleWinPercentage, redWinPercentage)
+
+		greatestCommonDivider = gcd(redChance, purpleChance)
+
+		return "%i : %i" % (redChance / greatestCommonDivider, purpleChance / greatestCommonDivider)
+
+	def _getPercentage(self, i1, i2):
+		return math.floor((i1 / (i1 + i2)) * 100)
 
 GAME_ODDS_SERVICE = GameOddsService()
