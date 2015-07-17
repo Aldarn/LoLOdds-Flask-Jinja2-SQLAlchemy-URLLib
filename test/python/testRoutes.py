d196524d8485b6634c84c626bd4c98e806cb1518
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 14:55:18 +0100

    Updated index route test to take into account current game odds data.

--- test/python/testRoutes.py
+++ test/python/testRoutes.py
@@ -6,11 +6,13 @@ import src.hextech_project_x as hpx
 
 class TestRoutes(unittest.TestCase):
 	@mock.patch('src.hextech_project_x.render_template')
-	def testIndex(self, render_template):
+	@mock.patch('src.hextech_project_x.GAME_ODDS_SERVICE.getGamesWithOdds')
+	def testIndex(self, getGameOddsMock, render_template):
+		getGameOddsMock.return_value = {}
 		# -------------------------------------------------------
 		result = hpx.index()
 		# -------------------------------------------------------
-		render_template.assert_called_with('index.html')
+		render_template.assert_called_with('index.html', currentGameOdds = {})
 
 def main():
 	unittest.main()
