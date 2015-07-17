fb1d5c4edd384add6850d10e0c76c3aa72e78b96
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 00:22:31 +0100

    Added team id and team name from id utils method.

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
-from resources.config import PROFILE_ICON_BASE_URL, CHAMPION_BASE_URL
+from resources.config import PROFILE_ICON_BASE_URL, CHAMPION_BASE_URL, BLUE_TEAM_ID, PURPLE_TEAM_ID
 
 def getProfileIconUrl(iconId):
 	return "%s%s.png" % (PROFILE_ICON_BASE_URL, iconId)
 
 def getChampionImageUrl(championName):
 	return "%s%s" % (CHAMPION_BASE_URL, championName)
+
+def getTeamName(teamId):
+	if teamId == BLUE_TEAM_ID:
+		return "BLUE"
+	elif teamId == PURPLE_TEAM_ID:
+		return "PURPLE"
+	return "UNKNOWN"
