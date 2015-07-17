4476c54acfac1f180450d3244579db7dd14172fa
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 23:44:14 +0100

    Updated ProcessParticipantTask to process new GameSummoners objects and updated Summoners objects.

--- src/api_daemon/tasks/process_participant_task.py
+++ src/api_daemon/tasks/process_participant_task.py
@@ -4,6 +4,7 @@ from task import Task
 from src.hextech_project_x import DB
 from src.api.summoner.summoner_by_name import SUMMONER_BY_NAME
 from src.api.stats.ranked_stats import RANKED_STATS
+from src.domain.game_summoners import GameSummoners
 from src.domain.summoners import Summoners
 from process_summoner_champion_task import ProcessSummonerChampionTask
 from src.utils import getProfileIconUrl
@@ -37,22 +38,17 @@ class ProcessParticipantTask(Task):
 		else:
 			print "got summoner %s" % summonerJSON
 
-			# Get the current champion image URL
-			# TODO: This should really be part of another system that maintains up to date links to static content,
-			# but it'll do as a quick hack
-			championImageUrl = getChampionImageUrl(CHAMPION_BY_ID.getChampionImageName(self.championId))
-
 			# For brevity - get the dictionary for the summoner name (there's only ever one key which corresponds
 			# to the summoner name)
 			summonerJSON = summonerJSON[summonerJSON.keys()[0]]
 
 			# Update the existing summoner from the JSON
 			if summoner:
-				self.updateExistingSummoner(summoner, summonerJSON, championImageUrl)
+				self._updateExistingSummoner(summoner, summonerJSON)
 
 			# Create a new summoner from the JSON
 			else:
-				summoner = self.save(summonerJSON, championImageUrl)
+				summoner = self.save(summonerJSON)
 
 			# Process the ranked stats
 			self.processRankedStats(summoner)
@@ -74,28 +70,40 @@ class ProcessParticipantTask(Task):
 			# TODO: What should we do about it?
 			print "Failed to get summoner ranked stats JSON, got: %s" % rankedStatsJSON
 		else:
-			# Check if we need to update the stats
-			lastStatsModified = rankedStatsJSON["modifyDate"]
-			if lastStatsModified > summoner.lastStatsModified:
-				# Save the modified date
-				summoner.lastStatsModified = lastStatsModified
-
-				self.updateRankedStats(summoner, rankedStatsJSON)
+			self.updateRankedStats(summoner, rankedStatsJSON)
 
 	"""
 	Updates the summoners ranked stats if appropriate, and spawns tasks to update the summoners
 	champion stats.
 	"""
 	def updateRankedStats(self, summoner, rankedStatsJSON):
+		lastStatsModified = rankedStatsJSON["modifyDate"]
+
+		# Initialize some stats we will collect
+		totalSessionsWon = 0
+		totalSessionsLost = 0
+		totalGameChampionSessionsWon = 0
+		totalGameChampionSessionsLost = 0
+
 		# Update all the stats
 		for championJSON in rankedStatsJSON["champions"]:
 			# Champion id 0 is an aggregate of all stats - we use this for the summoner object
 			if championJSON["id"] == 0:
-				summoner.totalSessionsWon = championJSON["stats"]["totalSessionsWon"]
-				summoner.totalSessionsLost = championJSON["stats"]["totalSessionsLost"]
+				totalSessionsWon = championJSON["stats"]["totalSessionsWon"]
+				totalSessionsLost = championJSON["stats"]["totalSessionsLost"]
 			else:
-				summonerChampionTask = ProcessSummonerChampionTask(championJSON, summoner)
-				summonerChampionTask.run()
+				# Store the stats for the current champion id
+				if championJSON["id"] == self.championId:
+					totalGameChampionSessionsWon = championJSON["stats"]["totalSessionsWon"]
+					totalGameChampionSessionsLost = championJSON["stats"]["totalSessionsLost"]
+				self._updateSummonerChampionStats(lastStatsModified, summoner, championJSON)
+
+		# Add the GameSummoners entry
+		self.saveGameSummoner(summoner.summonerId, totalSessionsWon, totalSessionsLost, totalGameChampionSessionsWon,
+			totalGameChampionSessionsLost)
+
+		# Update the summoner if necessary
+		self._updateSummonerStats(lastStatsModified, totalSessionsWon, totalSessionsLost)
 
 	"""
 	Gets an existing summoner object from the database.
@@ -111,21 +119,17 @@ class ProcessParticipantTask(Task):
 	"""
 	Creates a brand new summoner domain object.
 	"""
-	def save(self, summonerJSON, championImageUrl):
+	def save(self, summonerJSON):
 		# Last stats modified and total wins / losses will be set by the champion with id 0 once we grab them shortly
 		summoner = Summoners(int(summonerJSON["id"]), summonerJSON["name"], getProfileIconUrl(summonerJSON["profileIconId"]),
-			int(summonerJSON["revisionDate"]), 0, int(summonerJSON["summonerLevel"]), 0, 0, int(self.teamId),
-			int(self.championId), championImageUrl)
-
-		# Add it to the game so we get a relation
-		self.game.summoners.append(summoner)
+			int(summonerJSON["revisionDate"]), 0, int(summonerJSON["summonerLevel"]), 0, 0)
 
 		return summoner
 
 	"""
 	Updates an existing summoner object if necessary.
 	"""
-	def updateExistingSummoner(self, summoner, summonerJSON, championImageUrl):
+	def _updateExistingSummoner(self, summoner, summonerJSON):
 		# Add it to the game so we get a relation
 		self.game.summoners.append(summoner)
 
@@ -135,6 +139,39 @@ class ProcessParticipantTask(Task):
 			summoner.iconImageUrl = getProfileIconUrl(summonerJSON["profileIconId"])
 			summoner.lastModified = newLastModified
 			summoner.level = int(summonerJSON["summonerLevel"])
-			summoner.teamId = int(self.teamId)
-			summoner.championId = int(self.championId)
-			summoner.championImageUrl = championImageUrl
+
+	"""
+	Updates the summoner stats if necessary.
+	"""
+	def _updateSummonerStats(self, lastStatsModified, summoner, totalSessionsWon, totalSessionsLost):
+		# Check if we need to update the summoner stats
+		if lastStatsModified > summoner.lastStatsModified:
+			# Save the modified date
+			summoner.lastStatsModified = lastStatsModified
+			summoner.totalSessionsWon = totalSessionsWon
+			summoner.totalSessionsLost = totalSessionsLost
+
+	"""
+	Updates the summoner champion stats if necessary.
+	"""
+	def _updateSummonerChampionStats(self, lastStatsModified, summoner, championJSON):
+		if lastStatsModified > summoner.lastStatsModified:
+			summonerChampionTask = ProcessSummonerChampionTask(championJSON, summoner)
+			summonerChampionTask.run()
+
+	"""
+	Saves the GameSummoner for this game and summoner with the relevant stats and champion image url.
+	"""
+	def saveGameSummoner(self, summoner, totalSessionsWon, totalSessionsLost, totalGameChampionSessionsWon,
+			totalGameChampionSessionsLost):
+		# Get the current champion image URL
+		# TODO: This should really be part of another system that maintains up to date links to static content,
+		# but it'll do as a quick hack
+		championImageUrl = getChampionImageUrl(CHAMPION_BY_ID.getChampionImageName(self.championId))
+
+		# Create the GameSummoner domain object
+		gameSummoner = GameSummoners(self.game.gameId, summoner.summonerId, totalSessionsWon, totalSessionsLost,
+			totalGameChampionSessionsWon, totalGameChampionSessionsLost, self.teamId, self.championId, championImageUrl)
+
+		# Add the summoner reference
+		gameSummoner.summoner = summoner
