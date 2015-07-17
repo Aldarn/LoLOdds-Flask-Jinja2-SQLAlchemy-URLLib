48b8e40e476c780f3554aa5315afd8816756048a
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 22:20:18 +0100

    Began adding support for testing the database.

--- test/python/services/testGameOddsService.py
+++ test/python/services/testGameOddsService.py
@@ -1,10 +1,51 @@
 #!/usr/bin/env/python2.7
 
 import unittest
+from src.hextech_project_x import APP
 from src.services.game_odds_service import GAME_ODDS_SERVICE
+from flask.ext.sqlalchemy import SQLAlchemy
 
 class TestProcessSummonerChampionTask(unittest.TestCase):
-	# TODO: Setup test DB
+	# Set the test config for the APP
+	APP.config.from_object('test.test_config')
+
+	# Create a DB handle from the app
+	DB = SQLAlchemy(APP)
+
+	def setUp(self):
+		self.db = TestProcessSummonerChampionTask.DB
+		# TODO: Create db & tables
+		pass
+
+	def tearDown(self):
+		self.db.session.remove()
+		self.db.drop_all()
+
+	# def getGamesWithOdds(self):
+	# 	games = Games.query.all()
+	#
+	# 	gameList = []
+	# 	for game in games:
+	# 		teams = defaultdict(list)
+	# 		teamWinsAndLosses = defaultdict(lambda: defaultdict(int))
+	# 		for summoner in game.summoners:
+	# 			teamWinsAndLosses[summoner.teamId]["wins"] += summoner.totalSessionsWon
+	# 			teamWinsAndLosses[summoner.teamId]["losses"] += summoner.totalSessionsLost
+	# 			teams[summoner.teamId].append({
+	# 				"name": summoner.name,
+	# 				"championImageUrl": summoner.championImageUrl,
+	# 				"winRate": self._getPercentage(summoner.totalSessionsWon, summoner.totalSessionsLost)
+	# 			})
+	#
+	# 		# There's probably a better way of doing this, but at 3am i'm happy...
+	# 		odds = self._calculateGameOdds(teamWinsAndLosses[teamWinsAndLosses.keys()[0]]["wins"],
+	# 			teamWinsAndLosses[teamWinsAndLosses.keys()[0]]["losses"],
+	# 			teamWinsAndLosses[teamWinsAndLosses.keys()[1]]["wins"],
+	# 			teamWinsAndLosses[teamWinsAndLosses.keys()[1]]["losses"])
+	#
+	# 		gameList.append({ "teams": teams, "odds": odds, "mode": game.gameMode, "queue": game.gameQueueId })
+	#
+	# 	return gameList
 
 	def testGetGamesWithOdds(self):
 		# -------------------------------------------------------
---
+++ test/python/test_config.py
@@ -0,0 +1,5 @@
+# Run in debug mode for useful error messages
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = "mysql://root@localhost/testhextechprojectx?charset=utf8mb4"
+
