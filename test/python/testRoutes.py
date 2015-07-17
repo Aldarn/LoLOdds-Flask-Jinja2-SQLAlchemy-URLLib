9e113288c3950480cc9f6a7f0c50c04f0f8b5e39
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 22:19:52 +0100

    Added error route test.

--- test/python/testRoutes.py
+++ test/python/testRoutes.py
@@ -8,11 +8,20 @@ class TestRoutes(unittest.TestCase):
 	@mock.patch('src.hextech_project_x.render_template')
 	@mock.patch('src.hextech_project_x.GAME_ODDS_SERVICE.getGamesWithOdds')
 	def testIndex(self, getGameOddsMock, render_template):
-		getGameOddsMock.return_value = {}
+		getGameOddsMock.return_value = [{}]
 		# -------------------------------------------------------
 		result = hpx.index()
 		# -------------------------------------------------------
-		render_template.assert_called_with('index.html', currentGameOdds = {})
+		render_template.assert_called_with('index.html', currentGameOdds = [{}])
+
+	@mock.patch('src.hextech_project_x.render_template')
+	@mock.patch('src.hextech_project_x.GAME_ODDS_SERVICE.getGamesWithOdds')
+	def testIndexShowsNoGames(self, getGameOddsMock, render_template):
+		getGameOddsMock.return_value = []
+		# -------------------------------------------------------
+		result = hpx.index()
+		# -------------------------------------------------------
+		render_template.assert_called_with('nogames.html')
 
 def main():
 	unittest.main()
