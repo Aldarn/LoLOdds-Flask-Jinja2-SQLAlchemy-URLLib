281f090b31f0c44f99714f854afa7132e0974c2f
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 17:27:54 +0100

    Added tests for ProcessParticipantTask.

--- test/python/daemon/task/testProcessParticipantTask.py
+++ test/python/daemon/task/testProcessParticipantTask.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from mock import Mock, patch
 from src.api_daemon.tasks.process_participant_task import ProcessParticipantTask
 
 class TestProcessParticipantTask(unittest.TestCase):
@@ -71,6 +72,48 @@ class TestProcessParticipantTask(unittest.TestCase):
 
 		# -------------------------------------------------------
 		self.fail("didn't get current summoner with crazy name")
+
+	# def updateRankedStats(self, summoner, lastStatsModified, rankedStatsJSON):
+	# 	# Save the modified date
+	# 	summoner.lastStatsModified = lastStatsModified
+	#
+	# 	# Update all the stats
+	# 	for championJSON in rankedStatsJSON["champions"]:
+	# 		# Champion id 0 is an aggregate of all stats - we use this for the summoner object
+	# 		if championJSON["id"] == 0:
+	# 			summoner.totalSessionsWon = championJSON["stats"]["totalSessionsWon"]
+	# 			summoner.totalSessionsLost = championJSON["stats"]["totalSessionsLost"]
+	# 		else:
+	# 			summonerChampionTask = ProcessSummonerChampionTask(championJSON, summoner)
+	# 			summonerChampionTask.run()
+	#
+	# 	# Commit the changes
+	# 	self.update(summoner)
+
+	@patch.object(ProcessParticipantTask, 'update')
+	def testUpdateRankedStatsCallsUpdate(self, updateMock):
+		game = Mock()
+		summoner = Mock()
+		lastStatsModified = 1
+		rankedStatsJSON = { "champions": [] }
+		task = ProcessParticipantTask("name", 1, 2, game)
+		# -------------------------------------------------------
+		task.updateRankedStats(summoner, lastStatsModified, rankedStatsJSON)
+		# -------------------------------------------------------
+		updateMock.assert_called_with(summoner)
+
+	@patch.object(ProcessParticipantTask, 'update')
+	def testUpdateRankedStatsSetsSummonerStats(self, updateMock):
+		game = Mock()
+		summoner = Mock()
+		lastStatsModified = 1
+		rankedStatsJSON = { "champions": [{"id": 0, "stats": { "totalSessionsWon": 5, "totalSessionsLost": 50 } }] }
+		task = ProcessParticipantTask("name", 1, 2, game)
+		# -------------------------------------------------------
+		task.updateRankedStats(summoner, lastStatsModified, rankedStatsJSON)
+		# -------------------------------------------------------
+		self.assertEquals(summoner.totalSessionsWon, 5)
+		self.assertEquals(summoner.totalSessionsLost, 50)
 
 def main():
 	unittest.main()
