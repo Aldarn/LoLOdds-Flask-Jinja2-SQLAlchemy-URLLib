78a30a92e605f65e069a42e31e99179aa040e539
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 02:19:23 +0100

    Added comments to GameSummoners.

--- src/domain/game_summoners.py
+++ src/domain/game_summoners.py
@@ -1,5 +1,11 @@
 from src.hextech_project_x import DB
 
+# *** NOTE:
+#
+# This should contain team_id and champion_id, however Flask-SQLAlchemy appears not to support
+# "Associated" tables, or at least it is buggy. After several hours of trying to fix this i'm forced due to
+# the remaining time to hack this into the Summoners table instead.
+
 GAME_SUMMONERS = DB.Table(
 	'game_summoners',
     DB.Column('game_id', DB.BigInteger, DB.ForeignKey('games.gameId'), primary_key = True),
