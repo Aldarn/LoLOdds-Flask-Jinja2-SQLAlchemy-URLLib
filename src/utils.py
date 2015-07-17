01b89f1acbf0f9ee695f2b49cded37f774878056
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 14:48:40 +0100

    Added getChampionImageUrl method to utils.

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-from resources.config import PROFILE_ICON_BASE_URL
+from resources.config import PROFILE_ICON_BASE_URL, CHAMPION_BASE_URL
 
 def getProfileIconUrl(iconId):
 	return "%s%s.png" % (PROFILE_ICON_BASE_URL, iconId)
+
+def getChampionImageUrl(championName):
+	return "%s%s" % (CHAMPION_BASE_URL, championName)
