bbc5a508e9af4407640367781037611892ecd97e
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-17 22:17:48 +0100

    APIService now handles a lot of different network failures.

--- src/api/api_service.py
+++ src/api/api_service.py
@@ -8,6 +8,11 @@ import time
 class APIService(object):
 	__metaclass__ = abc.ABCMeta # This is an abstract base class and shouldn't be instantiated
 
+	RATE_LIMIT_HTTP_CODE = 429
+	SERVICE_UNAVAILABLE_CODE = 503
+	GATEWAY_TIMEOUT_CODE = 504
+	NO_INTERNET_ERROR_CODES = ["Errno 50", "Errno 8", "Errno 60"]
+
 	def __init__(self, endpointBase):
 		self._apiEndpointBase = endpointBase
 
@@ -30,18 +35,42 @@ class APIService(object):
 
 			# If the result has a status and status code that isn't 200, it failed
 			if "status" in result and "status_code" in result["status"] and result["status"]["status_code"] != 200:
-				return False, result
+				return self.__handleError(result)
 			# Anything else should be success
 			else:
 				return True, result
+
 		except urllib2.HTTPError, he:
 			# Code 429 is rate limit, sleep it off and try again
-			if he.code == 429:
+			# TODO: Handling this here maybe isn't the best idea if some service wants to react differently,
+			# but for now it's ok - YAGNI and all that
+			if he.code == APIService.RATE_LIMIT_HTTP_CODE:
 				print "hit rate limit, sleeping..."
 				time.sleep(config.RATE_LIMIT_TIMEOUT)
 				return self._getData(endpoint = endpoint, **params)
-			return False, he
+			elif he.code == APIService.SERVICE_UNAVAILABLE_CODE:
+				print "service unavailable, sleeping..."
+				time.sleep(config.SERVICE_UNAVAILABLE_TIMEOUT)
+				return self._getData(endpoint = endpoint, **params)
+			elif he.code == APIService.GATEWAY_TIMEOUT_CODE:
+				print "gateway timeout, sleeping..."
+				time.sleep(config.GATEWAY_TIMEOUT_TIMEOUT)
+				return self._getData(endpoint = endpoint, **params)
+			return self.__handleError(he)
+
+		except urllib2.URLError, urle:
+			# Handle cases of an internet connection drop
+			# I wish there was a nicer way to get this, but looking at the API it doesn't look like it's exposed
+			if any(noInternetErrorCode in urle.__str__() for noInternetErrorCode in APIService.NO_INTERNET_ERROR_CODES):
+				print "internet has died, sleeping..."
+				time.sleep(config.NO_INTERNET_TIMEOUT)
+				return self._getData(endpoint = endpoint, **params)
+			return self.__handleError(urle)
+
 		except Exception, e:
-			# TODO: Log error properly
-			print "Failed to get API data: %s" % e
-			return False, e
+			return self.__handleError(e)
+
+	def __handleError(self, e):
+		# TODO: Log error properly
+		print "Failed to get API data: %s" % e
+		return False, e
