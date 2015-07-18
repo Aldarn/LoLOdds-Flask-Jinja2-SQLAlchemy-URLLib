9d7d72a2590d58bab3853b03a249fa7efebe1f92
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 03:57:16 +0100

    Added latest SQL dump.

--- sql/hextechprojectx_2015-07-18.sql
+++ sql/hextechprojectx_2015-07-18.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.23)
 # Database: hextechprojectx
-# Generation Time: 2015-07-16 16:43:17 +0000
+# Generation Time: 2015-07-18 02:56:43 +0000
 # ************************************************************
 
 
@@ -26,12 +26,19 @@
 DROP TABLE IF EXISTS `game_summoners`;
 
 CREATE TABLE `game_summoners` (
-  `game_id` int(11) NOT NULL,
-  `summoner_id` int(11) NOT NULL,
-  PRIMARY KEY (`game_id`,`summoner_id`),
-  KEY `summoner_id` (`summoner_id`),
-  CONSTRAINT `game_summoners_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`gameId`),
-  CONSTRAINT `game_summoners_ibfk_2` FOREIGN KEY (`summoner_id`) REFERENCES `summoners` (`summonerId`)
+  `gameId` bigint(20) NOT NULL,
+  `summonerId` bigint(20) NOT NULL,
+  `totalSessionsWon` int(11) DEFAULT NULL,
+  `totalSessionsLost` int(11) DEFAULT NULL,
+  `totalChampionSessionsWon` int(11) DEFAULT NULL,
+  `totalChampionSessionsLost` int(11) DEFAULT NULL,
+  `teamId` int(11) DEFAULT NULL,
+  `championId` int(11) DEFAULT NULL,
+  `championImageUrl` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`gameId`,`summonerId`),
+  KEY `summonerId` (`summonerId`),
+  CONSTRAINT `game_summoners_ibfk_1` FOREIGN KEY (`gameId`) REFERENCES `games` (`gameId`),
+  CONSTRAINT `game_summoners_ibfk_2` FOREIGN KEY (`summonerId`) REFERENCES `summoners` (`summonerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -42,13 +49,18 @@ CREATE TABLE `game_summoners` (
 DROP TABLE IF EXISTS `games`;
 
 CREATE TABLE `games` (
-  `gameId` int(11) NOT NULL,
+  `gameId` bigint(20) NOT NULL,
   `gameMode` varchar(50) DEFAULT NULL,
   `gameQueueId` int(11) DEFAULT NULL,
   `gameType` varchar(50) DEFAULT NULL,
   `mapId` int(11) DEFAULT NULL,
-  `platformId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`gameId`)
+  `platformId` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`gameId`),
+  KEY `ix_games_gameType` (`gameType`),
+  KEY `ix_games_platformId` (`platformId`),
+  KEY `ix_games_gameMode` (`gameMode`),
+  KEY `ix_games_mapId` (`mapId`),
+  KEY `ix_games_gameQueueId` (`gameQueueId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -59,12 +71,12 @@ CREATE TABLE `games` (
 DROP TABLE IF EXISTS `summoner_champion_stats`;
 
 CREATE TABLE `summoner_champion_stats` (
-  `summonerId` int(11) NOT NULL,
-  `championId` int(11) NOT NULL,
-  `championImageUrl` varchar(255) DEFAULT NULL,
+  `summonerId` bigint(20) NOT NULL,
+  `championId` bigint(20) NOT NULL,
   `totalSessionsWon` int(11) DEFAULT NULL,
   `totalSessionsLost` int(11) DEFAULT NULL,
-  PRIMARY KEY (`summonerId`,`championId`)
+  PRIMARY KEY (`summonerId`,`championId`),
+  CONSTRAINT `summoner_champion_stats_ibfk_1` FOREIGN KEY (`summonerId`) REFERENCES `summoners` (`summonerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -75,14 +87,20 @@ CREATE TABLE `summoner_champion_stats` (
 DROP TABLE IF EXISTS `summoners`;
 
 CREATE TABLE `summoners` (
-  `summonerId` int(11) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `iconId` int(11) DEFAULT NULL,
-  `lastModified` int(11) DEFAULT NULL,
+  `summonerId` bigint(20) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `platformId` varchar(10) DEFAULT NULL,
+  `iconImageUrl` varchar(255) DEFAULT NULL,
+  `lastModified` bigint(20) DEFAULT NULL,
+  `lastStatsModified` bigint(20) DEFAULT NULL,
   `level` int(11) DEFAULT NULL,
   `totalSessionsWon` int(11) DEFAULT NULL,
   `totalSessionsLost` int(11) DEFAULT NULL,
-  PRIMARY KEY (`summonerId`)
+  PRIMARY KEY (`summonerId`),
+  KEY `ix_summoners_name` (`name`),
+  KEY `ix_summoners_lastModified` (`lastModified`),
+  KEY `ix_summoners_lastStatsModified` (`lastStatsModified`),
+  KEY `ix_summoners_platformId` (`platformId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
