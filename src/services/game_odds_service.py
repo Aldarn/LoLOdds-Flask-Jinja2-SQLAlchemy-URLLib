7809be896514b1fba723f898778e6a5a57c3bc69
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 15:15:24 +0100

    Added additional method comments to GameOddsService and now ordering games descending.

--- src/services/game_odds_service.py
+++ src/services/game_odds_service.py
@@ -6,8 +6,27 @@ class GameOddsService(object):
 	DEFAULT_ODDS = "1 : 1"
 	DEFAULT_PERCENTAGE = 50
 
+	"""
+	Cumulates the total (and current champion) wins and losses for each team for each game, calculates
+	the odds based on these totals and records all the data along with team summoner information in
+	a dictionary for passing to the UI to display.
+
+	NOTE: This currently calculates odds based on total values per team, i.e:
+
+	all team summoners wins / (all team summoners wins + all team summoners losses)
+
+	It may be more meaningful to do this on a player by player basis using their individual win
+	rates to get a team win rate average, i.e:
+
+	(player1 wins / (player1 wins + player1 losses) + ... + playerN wins / (playerN wins + playerN losses)) * team size
+
+	Then the odds would be calculated by comparing both team's win rate averages. This would give more weight
+	towards individual players, making weak and strong players have a greater skew of the odds.
+	"""
 	def getGamesWithOdds(self):
-		games = Games.query.all()
+		# Grab all the games, latest first
+		# TODO: Add a date/time field to the Games table and order on that
+		games = Games.query.order_by(Games.gameId.desc()).all()
 
 		gameList = []
 		for game in games:
@@ -57,6 +76,10 @@ class GameOddsService(object):
 
 		return gameList
 
+	"""
+	Gets the win percentage for each team, finds their relative chances of winning and
+	then finds the greatest common divisor to convert to the odds ratio.
+	"""
 	def _calculateGameOdds(self, blueTotalWins, blueTotalLosses, purpleTotalWins, purpleTotalLosses):
 		blueWinPercentage = self._getPercentage(blueTotalWins, blueTotalLosses)
 		purpleWinPercentage = self._getPercentage(purpleTotalWins, purpleTotalLosses)
@@ -64,11 +87,11 @@ class GameOddsService(object):
 		blueChance = self._getPercentage(blueWinPercentage, purpleWinPercentage)
 		purpleChance = self._getPercentage(purpleWinPercentage, blueWinPercentage)
 
-		greatestCommonDivider = gcd(blueChance, purpleChance)
-		if greatestCommonDivider == 0:
-			greatestCommonDivider = 1
+		greatestCommonDivisor = gcd(blueChance, purpleChance)
+		if greatestCommonDivisor == 0:
+			greatestCommonDivisor = 1
 
-		return "%i : %i" % (blueChance / greatestCommonDivider, purpleChance / greatestCommonDivider)
+		return "%i : %i" % (blueChance / greatestCommonDivisor, purpleChance / greatestCommonDivisor)
 
 	def _getPercentage(self, i1, i2):
 		# In this context, if both values are 0 that means there's no data on wins or losses, so lets call
