8a28be9ced75d7677be620b6b1f6246ca485e755
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 04:29:33 +0100

    Added tests for ProcessFeaturedGameTask.

--- test/python/daemon/task/testProcessFeaturedGameTask.py
+++ test/python/daemon/task/testProcessFeaturedGameTask.py
@@ -2,37 +2,59 @@
 
 import unittest
 from src.api_daemon.tasks.process_featured_game_task import ProcessFeaturedGameTask
+from mock import Mock, patch
+from src.hextech_project_x import APP, DB
+from src.domain.games import Games
 
 class TestProcessFeaturedGameTask(unittest.TestCase):
-	def testRun(self):
-		# -------------------------------------------------------
+	def setUp(self):
+		APP.config.from_object('src.resources.test_config')
+		DB.session.close()
+		DB.drop_all()
+		DB.create_all()
 
-		# -------------------------------------------------------
-		self.fail("game object not as expected")
+		self.featuredGameJSON = {}
+		self.task = ProcessFeaturedGameTask(self.featuredGameJSON)
 
-	def testRunHandlesFailure(self):
-		# -------------------------------------------------------
+	@patch.object(ProcessFeaturedGameTask, 'save')
+	@patch('src.api_daemon.tasks.process_featured_game_task.ProcessParticipantTask')
+	def testRun(self, processParticipantTaskMock, saveMock):
+		self.task.featuredGameJSON = {"gameId": 1, "gameMode": "ARAM", "gameQueueConfigId": 4, "gameType": "MATCHED",
+			"mapId": 1, "platformId": "EUW1", "participants": [{"summonerName": "soSleepy", "teamId": 100, "championId": 1},
+			{"summonerName": "soClose", "teamId": 200, "championId": 56}]}
 
+		processParticipantTaskMock.side_effect = lambda *args, **kwargs: Mock()
 		# -------------------------------------------------------
-		self.fail("failure not handled")
-
-	def testRunHandlesEmptyResults(self):
+		self.task.run()
 		# -------------------------------------------------------
-
-		# -------------------------------------------------------
-		self.fail("empty results not handled")
+		self.assertEquals(processParticipantTaskMock.call_count, 2)
+		self.assertTrue(saveMock.called)
 
 	def testSave(self):
+		game = Games(1, "mode", 2, "type", 3, "EUW1")
 		# -------------------------------------------------------
+		self.task.save(game)
+		# -------------------------------------------------------
+		gameResults = Games.query.all()
+		self.assertEquals(len(gameResults), 1)
+		self.assertEquals(gameResults[0].gameId, 1)
+		self.assertEquals(gameResults[0].gameMode, "mode")
+		self.assertEquals(gameResults[0].gameQueueId, 2)
+		self.assertEquals(gameResults[0].gameType, "type")
+		self.assertEquals(gameResults[0].mapId, 3)
+		self.assertEquals(gameResults[0].platformId, "EUW1")
 
-		# -------------------------------------------------------
-		self.fail("didn't save properly")
+	@patch.object(ProcessFeaturedGameTask, 'save')
+	def testExistingGameNotSaved(self, saveMock):
+		game = Games(1, "mode", 2, "type", 3, "EUW1")
+		DB.session.add(game)
+		DB.session.commit()
 
-	def testExistingGameNotSaved(self):
+		self.task.featuredGameJSON = {"gameId": 1}
 		# -------------------------------------------------------
-
+		self.task.run()
 		# -------------------------------------------------------
-		self.fail("existing game object should not be saved")
+		self.assertFalse(saveMock.called)
 
 def main():
 	unittest.main()
