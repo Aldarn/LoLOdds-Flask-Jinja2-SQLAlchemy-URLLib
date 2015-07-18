ceae900e272eedd410eacea404e5d0cc8cd75faa
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-07-18 19:18:05 +0100

    Small fix to ProcessSummonerChampionTask appending object after adding to session.

--- src/api_daemon/tasks/process_summoner_champion_task.py
+++ src/api_daemon/tasks/process_summoner_champion_task.py
@@ -13,8 +13,6 @@ class ProcessSummonerChampionTask(Task):
 	def run(self):
 		championId = int(self.championJSON["id"])
 
-		# print "got championId %s for summoner %s" % (championId, self.summoner.summonerId)
-
 		# Check if these stats have already been recorded
 		with DB.session.no_autoflush:
 			currentChampionStats = SummonerChampionStats.query.filter_by(summonerId = self.summoner.summonerId,
@@ -30,15 +28,17 @@ class ProcessSummonerChampionTask(Task):
 	Saves a new entry.
 	"""
 	def save(self, championId):
+		print "saving new summoner champion stats"
 		summonerChampionStats = SummonerChampionStats(self.summoner.summonerId, championId,
 			self.championJSON["stats"]["totalSessionsWon"], self.championJSON["stats"]["totalSessionsLost"])
 
-		self.summoner.summonerChampionStats.append(summonerChampionStats)
 		DB.session.add(summonerChampionStats)
+		self.summoner.summonerChampionStats.append(summonerChampionStats)
 
 	"""
 	Updates an existing entry.
 	"""
 	def updateExistingChampionStats(self, currentChampionStats):
+		print "updating existing summoner champion stats"
 		currentChampionStats.totalSessionsWon = self.championJSON["stats"]["totalSessionsWon"]
 		currentChampionStats.totalSessionsLost = self.championJSON["stats"]["totalSessionsLost"]
